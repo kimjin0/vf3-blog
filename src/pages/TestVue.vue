d8c9a3ea03a5d7a15a590d81fc80527e74980f2a
kimjin0 <pkimjin0q@gmail.com>
2022-10-11 15:16:35 +0900

    vuejs test

---
+++ src/pages/TestVue.vue
@@ -0,0 +1,29 @@
+<!-- eslint-disable no-tabs -->
+<template>
+	<div id="event-handling">
+		<p>{{ message }}</p>
+		<button v-on:click="reverseMessage">Reverse Message</button>
+	</div>
+</template>
+<script lag="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TestVue',
+
+  data () {
+    return {
+      message: 'Hello Vue.js!'
+    }
+  },
+  methods: {
+    reverseMessage () {
+      this.message = this.message
+        .split('')
+        .reverse()
+        .join('')
+    }
+  }
+
+})
+</script>
