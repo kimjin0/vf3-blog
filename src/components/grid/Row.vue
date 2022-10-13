4d18d24f5965c974cd5d40de57ca4da1a964d29b
kimjin0 <pkimjin0q@gmail.com>
2022-10-13 16:17:23 +0900

    https://youtu.be/2n9piG7_zJE?list=PLjpTKic1SLZvwX7Cc6L2ILRz7qnC8Cqif&t=833 layout 및 select box

---
+++ src/components/grid/Row.vue
@@ -0,0 +1,38 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { ref } from 'vue'
+const justify = ref('justify-start')
+const justifys = [
+  'start',
+  'center',
+  'end',
+  'aroud',
+  'between',
+  'evenly'
+].map(v => 'justify-' + v)
+</script>
+<template>
+    <q-card>
+        <q-toolbar>
+            <q-toobar-title>row</q-toobar-title>
+            <q-select
+                v-model="justify"
+                :options="justifys"
+            />
+        </q-toolbar>
+        <q-card-select>
+            {{ justify }}, {{  justifys }}
+        </q-card-select>
+        <div class="row" :class="justify">
+            <div v-for="i in 7" :key="i">
+                <q-avatar
+                    size="100px"
+                    font-size="52px"
+                    color="teal"
+                    text-color="white"
+                    icon="directions"
+                />
+            </div>
+        </div>
+    </q-card>
+</template>
---
+++ src/pages/BlogVue.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Row from 'components/grid/Row.vue'
+</script>
+
+<template>
+    <Row />
+</template>
