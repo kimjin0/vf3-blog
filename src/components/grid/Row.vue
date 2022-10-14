27cb5c51ee3f542581d97fcbbda24ae134a4130a
kimjin0 <pkimjin0q@gmail.com>
2022-10-14 15:08:23 +0900

    Grid Row

--- src/components/grid/Row.vue
+++ src/components/grid/Row.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 const justify = ref('justify-start')
 const justifys = [
   'start',
@@ -10,25 +10,33 @@ const justifys = [
   'between',
   'evenly'
 ].map(v => 'justify-' + v)
+
+const item = ref('items-start')
+const items = ['items-start', 'items-center', 'items-end']
+const rowClass = computed(() => [justify.value, item.value].join(' '))
 </script>
 <template>
     <q-card>
         <q-toolbar>
-            <q-toobar-title>row</q-toobar-title>
+            <!-- <q-toobar-title>row</q-toobar-title> -->
             <q-select
                 v-model="justify"
                 :options="justifys"
             />
+            <q-select
+                v-model="item"
+                :options="items"
+            />
         </q-toolbar>
         <q-card-select>
-            {{ justify }}, {{  justifys }}
+           {{  rowClass }}
         </q-card-select>
         <div class="row"
-            :class="justify"
+            :class="rowClass"
         >
             <div v-for="i in 7" :key="i">
                 <q-avatar
-                    size="100px"
+                    :size="`${i * 10 + 30}px`"
                     font-size="52px"
                     color="teal"
                     text-color="white"
